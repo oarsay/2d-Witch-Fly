3fc764e41b6321acc0d426a97b7bf25b7963acc2
Oguzhan Elkapmis <oguzhan.elkapmis@gmail.com>
2023-02-20 22:57:44 +0300

    Add player vertical movement (via user input)
    - User touch input controls the player's vertical movement
    
    - DirectionVertical property was renamed to IsDiving.
    
    - Unlike horizontal boundary, the vertical boundary needs two different variables - one for the bottom, one for the upper boundary. Because these two boundaries will have different numerical values.
    Therefore, _screenBoundaryVerticalUpper and _screenBoundaryVerticalBottom variables were created.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,31 +3,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     //************* HORIZONTAL MOVEMENT *************
-    private readonly float _screenBoundryHorizontal = 16f; //Witch patrols between -16 and +16 on the X-axis
+    private readonly float _screenBoundaryHorizontal = 16f; //Witch patrols between -16 and +16 on the X-axis
     private float _moveSpeedHorizontal = 3f;
     private bool DirectionHorizontal => (_moveSpeedHorizontal > 0); //false for left, true for right
 
     //************* VERTICAL MOVEMENT *************
-    private float _moveSpeedVertical = 1f;
-    private bool DirectionVertical => (_moveSpeedVertical < 0); //false for rising, true for diving
+    private readonly float _screenBoundaryVerticalUpper = 4f; //Witch can move between -2 and +4 on the Y-axis
+    private readonly float _screenBoundaryVerticalBottom = -2f;
+    private float _moveSpeedVertical = 2f;
+    private bool IsDiving => (_moveSpeedVertical < 0); //false for rising, true for diving
 
     void Update()
     {
+        ReadUserInput();
         Move();
-        CheckForBoundaries();
+    }
+    private void ReadUserInput()
+    {
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonUp(0))
+        {
+            ChangeDirection(ref _moveSpeedVertical);
+        }
     }
     private void Move()
     {
         // Move horizontal
-        Vector3 moveAmount = new(_moveSpeedHorizontal, 0f, 0f);
-        transform.position += (moveAmount * Time.deltaTime);
-    }
-
-    private void CheckForBoundaries()
-    {
         if (IsOutOfHorizontalBoundary())
         {
             ChangeDirection(ref _moveSpeedHorizontal);
+        }
+        else
+        {
+            Vector3 moveAmount = new(_moveSpeedHorizontal, 0f, 0f);
+            transform.position += (moveAmount * Time.deltaTime);
+        }
+
+        // Move vertical
+        if (!IsOutOfVerticalBoundary())
+        {
+            Vector3 moveAmount = new(0f, _moveSpeedVertical, 0f);
+            transform.position += (moveAmount * Time.deltaTime);
         }
     }
     private bool IsOutOfHorizontalBoundary()
@@ -37,16 +52,21 @@ public class PlayerMovement : MonoBehaviour
         // Because if we ignore its direction, once it is out of boundry it will continuously assume
         // that the witch reached the boundary and it will change witch's direction continuously.
 
-        // If moving to the left and out of left boundary
-        if ((transform.position.x < -_screenBoundryHorizontal) && !DirectionHorizontal) return true;
-        // If moving to the right and out of right boundary
-        if ((transform.position.x > _screenBoundryHorizontal) && DirectionHorizontal) return true;
+        // If it is moving to the left and out of left boundary
+        if ((transform.position.x < -_screenBoundaryHorizontal) && !DirectionHorizontal) return true;
+        // If it is moving to the right and out of right boundary
+        if ((transform.position.x > _screenBoundaryHorizontal) && DirectionHorizontal) return true;
 
         return false;
     }
 
     private bool IsOutOfVerticalBoundary()
     {
+        // If it is diving and out of bottom boundary
+        if ((transform.position.y < _screenBoundaryVerticalBottom) && IsDiving) return true;
+        // If it is rising and out of upper boundary
+        if ((transform.position.y > _screenBoundaryVerticalUpper) && !IsDiving) return true;
+
         return false;
     }
 
@@ -54,4 +74,6 @@ public class PlayerMovement : MonoBehaviour
     {
         speed *= -1;
     }
+
+    
 }
